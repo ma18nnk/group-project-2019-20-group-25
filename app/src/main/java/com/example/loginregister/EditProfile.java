60e1e88958bfc374b7a77a910e03a19e459a360f
dominykasulc <56392834+dominykasulc@users.noreply.github.com>
2020-01-31 12:44:40 +0000

    added scrollview and buttons

--- app/src/main/java/com/example/loginregister/EditProfile.java
+++ app/src/main/java/com/example/loginregister/EditProfile.java
@@ -5,20 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class EditProfile extends AppCompatActivity {
 
+public class EditProfile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
     }
 
+
     public void onSaveButtonClick(View view){
-        //write stuff to database
         finish();
     }
     public void onExitButtonClick(View view){
-
         finish();
     }
 
